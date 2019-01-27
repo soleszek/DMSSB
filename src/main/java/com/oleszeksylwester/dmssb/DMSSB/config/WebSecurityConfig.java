75776661bf334f213316c45fcf87030240fd5d56
Sylwester Oleszek <sylwester@localhost.localdomain>
2019-01-27 09:37:21 +0100

    Wo inheritance

--- src/main/java/com/oleszeksylwester/dmssb/DMSSB/config/WebSecurityConfig.java
+++ src/main/java/com/oleszeksylwester/dmssb/DMSSB/config/WebSecurityConfig.java
@@ -1,29 +1,38 @@
 package com.oleszeksylwester.dmssb.DMSSB.config;
 
+import com.oleszeksylwester.dmssb.DMSSB.service.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import javax.sql.DataSource;
-
 @Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    /*@Autowired
-    UserDetailsService userDetailsService;*/
+
+
+    @Autowired
+    private CustomUserDetailsService customUserDetailsService;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(customUserDetailsService).passwordEncoder(passwordEncoder());
+    }
 
     /*@Autowired
     public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception{
-        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+        auth.customUserDetailsService(customUserDetailsService).passwordEncoder(passwordEncoder());
     };*/
 
-    @Autowired
+    /*@Autowired
     private DataSource dataSource;
 
     @Autowired
@@ -32,9 +41,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         .usersByUsernameQuery(
                 "select username, password, enabled from dmsobject where username=?")
         .authoritiesByUsernameQuery(
-                "select username, role from user_role where username=?")
+                "select username, role from role where username=?")
         .passwordEncoder(new BCryptPasswordEncoder());
-    }
+    }*/
 
     @Override
     protected void configure(HttpSecurity http) throws Exception{
