35335d4f0b87e3667643bdbdc1fe72a53061df16
Sylwester Oleszek <sylwester@localhost.localdomain>
2019-02-02 20:20:55 +0100

    Initial code for tests

--- src/test/java/com/oleszeksylwester/dmssb/DMSSB/DmssbApplicationTests.java
+++ src/test/java/com/oleszeksylwester/dmssb/DMSSB/DmssbApplicationTests.java
@@ -2,8 +2,12 @@ package com.oleszeksylwester.dmssb.DMSSB;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -11,6 +15,7 @@ public class DmssbApplicationTests {
 
 	@Test
 	public void contextLoads() {
+
 	}
 
 }
